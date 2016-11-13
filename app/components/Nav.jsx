5fecc4d48cc0f5ba40aae9f051c2509c65ad172a
asm-morshed <mm.morshed28707@gmail.com>
2016-11-13 18:05:21 +0600

    after adding foundation

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,14 +2,38 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
+
+  onSearch: function (e) {
+    e.preventDefault();
+    console.log('on search');
+  },
   render: function () {
     return (
-      <div>
-        <h2>Nav Component</h2>
-        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
-        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-      </div>
+        <div className="top-bar">
+            <div className="top-bar-left">
+              <ul className="menu">
+                  <li>
+                    <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                  </li>
+                  <li>
+                    <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
+                  </li>
+                  <li>
+                    <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+                  </li>
+              </ul>
+            </div>
+            <div className="top-bar-right">
+              <form onSubmit={this.onSearch}>
+
+              <ul className="menu">
+                  <li><input type="search" placeholder="Search"/></li>
+                  <li><button type="submit" className="button">Search</button></li>
+
+              </ul>
+              </form>
+            </div>
+        </div>
     );
   }
 });
